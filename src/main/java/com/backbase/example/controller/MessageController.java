b5d1b3d8bb830d22144b7b678d856ee558d0bff0
erdny <www.erdnymaster@gmail.com>
2020-01-23 10:50:34 +0300

    z

--- src/main/java/com/backbase/example/controller/MessageController.java
+++ src/main/java/com/backbase/example/controller/MessageController.java
@@ -1,21 +1,16 @@
 package com.backbase.example.controller;
 
 
-import com.backbase.buildingblocks.presentation.errors.BadRequestException;
-import com.backbase.buildingblocks.presentation.errors.InternalServerErrorException;
 import com.backbase.example.domain.Message;
 import com.backbase.example.service.MessageService;
 import com.backbase.example.service.TextChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/v1")
@@ -28,14 +23,14 @@ public class MessageController {
     @RequestMapping(value = "/greeting",
     method = {RequestMethod.GET})
     @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody String getGreeting() throws BadRequestException, InternalServerErrorException {
+    public @ResponseBody String getGreeting() {
         return "Hello, World";
     }
 
     @RequestMapping(value = "/message/add",
     method = {RequestMethod.POST})
     @ResponseStatus(HttpStatus.CREATED)
-    public void addMessage(@RequestBody String text, @RequestParam("chatName") String chatName) throws BadRequestException, InternalServerErrorException {
+    public void addMessage(@RequestBody String text, @RequestParam("chatName") String chatName)  {
         messageService.saveMessage(text, chatName);
     }
 
